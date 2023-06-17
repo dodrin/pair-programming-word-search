5455247edc71ff8e689a3d55f4ad7029dd1738b6
Rina Inada <indrn@icloud.com>
2023-06-17 22:15:36 +0000

    modify wordserch and test,work paired by Darilla and Rina

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Pair Programming Dariia & Rina
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  let wordIsFound = false;
+
+  if (letters.length > 0 && word.length > 0) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoinArray = [];
+
+    const rowLength = letters.length;
+    const colLength = letters[0].length;
+
+    for (let i = 0; i < colLength; i++) {
+      verticalJoinArray[i] = [];
+      for (let k = 0; k < rowLength; k++) {
+        verticalJoinArray[i].push(letters[k][i]);
+      }
+    }
+    const verticalJoin = verticalJoinArray.map(ls => ls.join(''));
+    const allLetters = horizontalJoin.concat(verticalJoin);
+
+    for (let l of allLetters) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+  }
+
+  return wordIsFound;
+};
+
+module.exports = wordSearch;
